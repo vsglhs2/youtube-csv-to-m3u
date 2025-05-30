65cfd27651db75e040b1eccad54b7c2490d6ecf0
vsglhs2 <vsglhs2@proton.me>
2025-05-31 02:40:04 +1000

    brought csv-importer (https://github.com/sadmann7/csv-importer)

--- src/app/pages/picker/Picker.tsx
+++ src/app/pages/picker/Picker.tsx
@@ -1,7 +1,8 @@
+import { TricksTable } from "@/shadcn/components/tricks-table";
 import type { FC } from "react";
 
 export const PickerPage: FC = () => {
     return (
-        'picker'
+        <TricksTable />
     );
 };
